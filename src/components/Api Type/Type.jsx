d76310c0a945b17c338618c0d86bc1430f05da92
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-01-27 00:14:45 -0500

    fix CSS

--- src/components/Api Type/Type.jsx
+++ src/components/Api Type/Type.jsx
@@ -45,7 +45,8 @@ function ApiType(props) {
                   return a.dex - b.dex;
                 }), pokemonNew]
               )
-            });
+            })
+            ;
         });
       });
   };
