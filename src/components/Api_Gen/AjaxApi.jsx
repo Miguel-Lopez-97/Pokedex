e6f9e0792c40f296bc81da6f7903ba2712eab7d3
Miguel-Lopez-97 <miguel.m.lopez.p@gmail.com>
2022-01-27 00:40:23 -0500

    change in AjaxApi

---
+++ src/components/Api_Gen/AjaxApi.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../header/Nav/navigation.css";
+
+export function AjaxApi() {
+  
+    return (
+      <div className="containerBanner">
+        <img src="https://i.pinimg.com/originals/06/5e/5e/065e5e9239f7d492dc3d64e57e84d6ed.jpg" alt="" />
+      </div>
+    );
+  };
--- src/components/Api_Gen/Gen.jsx
+++ src/components/Api_Gen/Gen.jsx
@@ -81,12 +81,3 @@ export function ApiGen(props) {
     </>
   );
 };
-
-export function AjaxApi() {
-  
-  return (
-    <div className="containerBanner">
-      <img src="https://i.pinimg.com/originals/06/5e/5e/065e5e9239f7d492dc3d64e57e84d6ed.jpg" alt="" />
-    </div>
-  );
-};
